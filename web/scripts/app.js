ee974267c0a007426b35a875af3cffecde6d1e9c
chuns <chuns.liu@foxmail.com>
2021-03-13 03:30:36 +0800

    update

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1,10 +1,5 @@
 "use strict";
 
 import {formatFooter} from "./modules/footer.js";
+
 formatFooter();
-
-function hello() {
-    alert("Hello!");
-}
-
-export {hello};
--- web/scripts/modules/footer.js
+++ web/scripts/modules/footer.js
@@ -6,15 +6,14 @@ function formatFooter() {
         req.open('GET', '/api/v1/miscs/now');
         req.responseType = 'json';
         req.onload = function () {
-            const nowStr = "现在是：" + req.response.data + " .";
+            const nowStr = `<p class="footer">` + req.response.data + "</p>";
             let footerEle = document.querySelector('footer');
-            footerEle.innerHTML = '<p><i class="far fa-bell"></i> 欢迎访问：<a href="https://github.com/chunsen-ok" target="_blank">Github</a>. <span>{0}</span></p>'.replace('{0}', nowStr);
+            footerEle.innerHTML = `<p class="footer"><i class="far fa-bell"></i> 欢迎访问：<a href="https://github.com/chunsen-ok" target="_blank">Github</a>.</p>` + nowStr;
         }
         req.send();
     } catch(e) {
         console.log(JSON.stringify(e));
     }
-
 }
 
 export {formatFooter};
